75164b78a7adddd8ffb2d7bb86f6750f04123581
Aastha Pareek <133286045+aasthapareek@users.noreply.github.com>
2025-08-24 14:35:01 +0530

    Update env.js

--- pages/api/env.js
+++ pages/api/env.js
@@ -1,5 +1,8 @@
-module.exports = (req, res) => {
+// pages/api/env.js - Amplify version of your Vercel code
+
+export default function handler(req, res) {
+  // Your exact same functionality, just with different export syntax
   res.json({
     all_environment: process.env
   });
-};
+}
